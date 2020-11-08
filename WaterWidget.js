ff39525a9121797b8447e433a2f7f8888f65d33c
wolli911 <73888643+wolli911@users.noreply.github.com>
2020-11-08 20:16:04 +0100

    Update WaterWidget.js

--- WaterWidget.js
+++ WaterWidget.js
@@ -1,5 +1,16 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
+// WaterWidget für alle bayerischen Flüsse
+//
+//Daten für Wassertemperatur, Wasserqualität und Wasserhöhe werden vom NID und HND ausgelesen.
+//
+// Copyright (C) 2020 by wolli911 <wolli@wolli.info>
+//
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+// IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+// OF THIS SOFTWARE.
 
 //Wassertemperatur ermitteln vom NID
 const data = await fetchData()
